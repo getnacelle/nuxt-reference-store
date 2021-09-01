35c4add9c53a28872010a95a88f2a89d3f623655
Alex Asche <alexcasche@gmail.com>
2021-09-01 12:23:17 -0400

    collection page start

---
+++ components/collection/CollectionGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="product">
+    {{ product }}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import useProductProvider from '~/composables/useProductProvider'
+export default defineComponent({
+  props: {
+    handle: {
+      type: String,
+      default: null
+    }
+  },
+  setup(props) {
+    const { getProducts } = useCollectionProvider()
+    const product = getProducts([props.handle])[0]
+    return { product }
+  }
+})
+</script>
