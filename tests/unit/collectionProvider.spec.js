7db7c256641fd506267eb5c5efdad78e225aae13
Alex Asche <alexcasche@gmail.com>
2021-09-03 13:08:46 -0400

    unit tests minus fetching

--- tests/unit/collectionProvider.spec.js
+++ tests/unit/collectionProvider.spec.js
@@ -3,11 +3,11 @@ import CollectionProvider from '~/providers/CollectionProvider'
 import collectionData from '~/utils/collectionData'
 require('dotenv').config()
 
-// const config = {
-//   nacelleId: process.env.NACELLE_SPACE_ID,
-//   nacelleToken: process.env.NACELLE_GRAPHQL_TOKEN,
-//   nacelleEndpoint: process.env.NACELLE_ENDPOINT
-// }
+const config = {
+  nacelleId: process.env.NACELLE_SPACE_ID,
+  nacelleToken: process.env.NACELLE_GRAPHQL_TOKEN,
+  nacelleEndpoint: process.env.NACELLE_ENDPOINT
+}
 
 const InjectedComponent = () => {
   return {
@@ -31,7 +31,7 @@ const WrapperComponent = ({ props = {} } = {}) => ({
   render: (h) => h(CollectionProvider, { props }, [h(InjectedComponent())])
 })
 
-describe('Colletion Provider', () => {
+describe('Collection Provider', () => {
   it('provides `products`, `collections`, `setCollections`, `addCollections`, `fetchCollections`, `removeCollections`, `clearCollections`, `getCollections` and `fetchCollectionProducts` to children', () => {
     const collectionProvider = mount(WrapperComponent())
     const injectedComponent = collectionProvider.findComponent({
@@ -50,142 +50,126 @@ describe('Colletion Provider', () => {
     )
   })
 
-  // it('provides products based on initial prop', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: productData } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   expect(injectedComponent.vm.products.value.length).toBe(2)
-  // })
+  it('initializes products & collections from props', () => {
+    const productProvider = mount(
+      WrapperComponent({ props: { collections: collectionData } })
+    )
+    const injectedComponent = productProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    expect(injectedComponent.vm.products.value.length).toBe(2)
+    expect(injectedComponent.vm.collections.value.length).toBe(2)
+  })
 
-  // it('initializes products from props', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: productData } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   expect(injectedComponent.vm.products.value.length).toBe(2)
-  //   expect(injectedComponent.vm.products.value.length).toBe(2)
-  // })
+  it('calls setCollections function to update collections', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: [collectionData[1]] } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'setCollections')
+    injectedComponent.vm.setCollections({ collections: collectionData })
+    expect(injectedComponent.vm.setCollections).toHaveBeenCalledTimes(1)
+    expect(injectedComponent.vm.collections.value.length).toBe(2)
+  })
 
-  // it('calls setProducts function to update products', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: [productData[1]] } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'setProducts')
-  //   injectedComponent.vm.setProducts({ products: productData })
-  //   expect(injectedComponent.vm.setProducts).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value.length).toBe(2)
-  // })
+  it('calls addCollections function to update collections', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: [collectionData[0]] } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'addCollections')
+    injectedComponent.vm.addCollections({ collections: [collectionData[1]] })
+    expect(injectedComponent.vm.addCollections).toHaveBeenCalledTimes(1)
+    expect(injectedComponent.vm.collections.value.length).toBe(2)
+  })
 
-  // it('calls addProducts function to update products', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: [productData[0]] } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'addProducts')
-  //   injectedComponent.vm.addProducts({ products: [productData[1]] })
-  //   expect(injectedComponent.vm.addProducts).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value.length).toBe(2)
-  // })
+  it('calls addCollections function to update collections w/o duplicates', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: [collectionData[1]] } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'addCollections')
+    injectedComponent.vm.addCollections({ collections: [collectionData[1]] })
+    expect(injectedComponent.vm.addCollections).toHaveBeenCalledTimes(1)
+    expect(injectedComponent.vm.collections.value.length).toBe(1)
+  })
 
-  // it('calls addProducts function to update products w/o duplicates', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: [productData[1]] } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'addProducts')
-  //   injectedComponent.vm.addProducts({ products: [productData[1]] })
-  //   expect(injectedComponent.vm.addProducts).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value.length).toBe(1)
-  // })
+  it('calls fetchCollections function to fetch collections', async () => {
+    const collectionProvider = mount(WrapperComponent({ props: { config } }))
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    /*
+      Not sure best way to mock this one
+    */
+  })
 
-  // it('calls fetchProducts function to fetch products', async () => {
-  //   const productProvider = mount(WrapperComponent({ props: { config } }))
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   /*
-  //     Not sure best way to mock this one
-  //   */
-  // })
+  it('calls removeCollections function to remove collections', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: collectionData } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'removeCollections')
+    injectedComponent.vm.removeCollections({
+      handles: [collectionData[1].handle]
+    })
+    expect(injectedComponent.vm.removeCollections).toHaveBeenCalledTimes(1)
+    expect(injectedComponent.vm.collections.value.length).toBe(1)
+  })
 
-  // it('calls removeProducts function to remove products', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: productData } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'removeProducts')
-  //   injectedComponent.vm.removeProducts({ handles: [productData[1].handle] })
-  //   expect(injectedComponent.vm.removeProducts).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value.length).toBe(1)
-  // })
+  it('calls clearCollections function to clear collections', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: collectionData } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'clearCollections')
+    injectedComponent.vm.clearCollections()
+    expect(injectedComponent.vm.clearCollections).toHaveBeenCalledTimes(1)
+    expect(injectedComponent.vm.collections.value.length).toBe(0)
+  })
 
-  // it('calls clearProducts function to clear products', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: productData } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'clearProducts')
-  //   injectedComponent.vm.clearProducts()
-  //   expect(injectedComponent.vm.clearProducts).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value.length).toBe(0)
-  // })
+  it('calls getCollections function to return collection by handle', () => {
+    const collectionProvider = mount(
+      WrapperComponent({ props: { collections: collectionData } })
+    )
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    jest.spyOn(injectedComponent.vm, 'getCollections')
+    const shirts = injectedComponent.vm.getCollections({
+      handles: ['shirts']
+    })
+    const shirtsAndHats = injectedComponent.vm.getCollections({
+      handles: ['shirts', 'hats']
+    })
+    const allProducts = injectedComponent.vm.getCollections({
+      handles: null
+    })
+    expect(shirts.length).toBe(1)
+    expect(shirts[0].handle).toBe('shirts')
+    expect(shirtsAndHats.length).toBe(2)
+    expect(shirtsAndHats[1].handle).toBe('hats')
+    expect(allProducts.length).toBe(2)
+    expect(injectedComponent.vm.getCollections).toHaveBeenCalledTimes(3)
+  })
 
-  // it('calls setSelectedVariant function to update product variant', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: [productData[0]] } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'setSelectedVariant')
-  //   injectedComponent.vm.setSelectedVariant({
-  //     product: productData[0],
-  //     options: productData[0].variants[0].selectedOptions
-  //   })
-  //   expect(injectedComponent.vm.setSelectedVariant).toHaveBeenCalledTimes(1)
-  //   expect(injectedComponent.vm.products.value[0].selectedVariant.name).toBe(
-  //     'blue-shirt'
-  //   )
-  // })
-
-  // it('calls getProducts function to return product by handle', () => {
-  //   const productProvider = mount(
-  //     WrapperComponent({ props: { products: productData } })
-  //   )
-  //   const injectedComponent = productProvider.findComponent({
-  //     name: 'InjectedComponent'
-  //   })
-  //   jest.spyOn(injectedComponent.vm, 'getProducts')
-  //   const shirts = injectedComponent.vm.getProducts({
-  //     handles: ['shirt']
-  //   })
-  //   const shirtsAndHats = injectedComponent.vm.getProducts({
-  //     handles: ['shirt', 'hat']
-  //   })
-  //   const allProducts = injectedComponent.vm.getProducts({
-  //     handles: null
-  //   })
-  //   expect(shirts.length).toBe(1)
-  //   expect(shirts[0].handle).toBe('shirt')
-  //   expect(shirtsAndHats.length).toBe(2)
-  //   expect(shirtsAndHats[1].handle).toBe('hat')
-  //   expect(allProducts.length).toBe(2)
-  //   expect(injectedComponent.vm.getProducts).toHaveBeenCalledTimes(3)
-  // })
+  it('calls fetchCollectionProducts function to fetch collection products', async () => {
+    const collectionProvider = mount(WrapperComponent({ props: { config } }))
+    const injectedComponent = collectionProvider.findComponent({
+      name: 'InjectedComponent'
+    })
+    /*
+      Not sure best way to mock this one
+    */
+  })
 })
