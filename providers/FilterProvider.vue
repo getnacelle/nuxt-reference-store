30776e2ba48d63574bd0fdfdcfb5ed34cd2ca7a1
KrisQ <kquemener1@gmail.com>
2021-09-14 09:11:19 -0500

    setup

---
+++ providers/FilterProvider.vue
@@ -0,0 +1,29 @@
+<template>
+  <div><slot /></div>
+</template>
+
+<script>
+// import { ref, provide } from '@nuxtjs/composition-api'
+
+export default {
+  props: {
+    inputData: {
+      type: Array,
+      required: true
+    },
+    propertyFilters: {
+      type: Array,
+      required: true
+    },
+    priceRangeFilters: {
+      type: Array,
+      required: true
+    }
+  },
+  setup(props) {
+    // TODO Add functions that provide/return filtered data
+    // NOTE A lot of the functionalities will have to come from RefinementFilters
+    // LINK ../components/nacelle/RefinementFilters.vue
+  }
+}
+</script>
