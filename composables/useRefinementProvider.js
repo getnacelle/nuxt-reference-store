43359108bd647eb52639947099b66efee43abe63
Alex Asche <alexcasche@gmail.com>
2021-09-17 16:53:50 -0400

    shrinks composable code (#26)
    chore(composables) - shrinks composable code

--- composables/useRefinementProvider.js
+++ composables/useRefinementProvider.js
@@ -1,17 +1,11 @@
 import { inject } from '@vue/composition-api'
 
 export default function useRefinementProvider() {
-  const filters = inject('filters')
-  const setupFilters = inject('setupFilters')
-  const activeFilters = inject('activeFilters')
-  const filteredData = inject('filteredData')
-  const clearFilters = inject('clearFilters')
-
   return {
-    filters,
-    setupFilters,
-    activeFilters,
-    filteredData,
-    clearFilters
+    filters: inject('filters'),
+    setupFilters: inject('setupFilters'),
+    activeFilters: inject('activeFilters'),
+    filteredData: inject('filteredData'),
+    clearFilters: inject('clearFilters')
   }
 }
