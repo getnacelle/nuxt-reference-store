41cd86c8aa920990d798f7cb750cf85c132612f2
Alex Asche <alexcasche@gmail.com>
2021-09-17 12:45:17 -0400

    shrinks composable code

--- composables/useCartProvider.js
+++ composables/useCartProvider.js
@@ -1,21 +1,13 @@
 import { inject } from '@vue/composition-api'
 
 export default function useCartProvider() {
-  const cart = inject('cart')
-  const addItem = inject('addItem')
-  const removeItem = inject('removeItem')
-  const updateItem = inject('updateItem')
-  const incrementItem = inject('incrementItem')
-  const decrementItem = inject('decrementItem')
-  const clearCart = inject('clearCart')
-
   return {
-    cart,
-    addItem,
-    removeItem,
-    updateItem,
-    incrementItem,
-    decrementItem,
-    clearCart
+    cart: inject('cart'),
+    addItem: inject('addItem'),
+    removeItem: inject('removeItem'),
+    updateItem: inject('updateItem'),
+    incrementItem: inject('incrementItem'),
+    decrementItem: inject('decrementItem'),
+    clearCart: inject('clearCart')
   }
 }
--- composables/useSpaceProvider.js
+++ composables/useSpaceProvider.js
@@ -1,29 +1,17 @@
 import { inject } from '@vue/composition-api'
 
 export default function useSpaceProvider() {
-  const id = inject('id')
-  const name = inject('name')
-  const domain = inject('domain')
-  const metafields = inject('metafields')
-  const linklists = inject('linklists')
-  const nacelleSdk = inject('nacelleSdk')
-  const getLocalizedLinks = inject('getLocalizedLinks')
-  const getMetatag = inject('getMetatag')
-  const getMetafieldsObj = inject('getMetafieldsObj')
-  const getMetafieldsByNamespace = inject('getMetafieldsByNamespace')
-  const getMetafield = inject('getMetafield')
-
   return {
-    id,
-    name,
-    domain,
-    metafields,
-    linklists,
-    nacelleSdk,
-    getLocalizedLinks,
-    getMetatag,
-    getMetafieldsObj,
-    getMetafieldsByNamespace,
-    getMetafield
+    id: inject('id'),
+    name: inject('name'),
+    domain: inject('domain'),
+    metafields: inject('metafields'),
+    linklists: inject('linklists'),
+    nacelleSdk: inject('nacelleSdk'),
+    getLocalizedLinks: inject('getLocalizedLinks'),
+    getMetatag: inject('getMetatag'),
+    getMetafieldsObj: inject('getMetafieldsObj'),
+    getMetafieldsByNamespace: inject('getMetafieldsByNamespace'),
+    getMetafield: inject('getMetafield')
   }
 }
