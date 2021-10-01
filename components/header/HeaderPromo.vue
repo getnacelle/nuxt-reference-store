2c7b8eac1226960f9c70354e8638f06638612eb0
Alex Asche <alexcasche@gmail.com>
2021-10-01 11:43:37 -0400

    header scaffolded

---
+++ components/header/HeaderPromo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="bg-indigo-600 h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8"
+  >
+    Get free delivery on orders over $100
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ components/nav/Nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-white">
+    <div
+      v-show="showNav"
+      class="fixed inset-0 flex z-40 lg:hidden"
+      role="dialog"
+      aria-modal="true"
+    >
+      <nav-overlay />
+      <nav-drawer />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, inject, watch } from "@nuxtjs/composition-api";
+import NavOverlay from "./NavOverlay.vue";
+
+export default {
+  components: { NavOverlay },
+  setup() {
+    const showNav = ref(false);
+    const navOpen = inject("navOpen");
+
+    watch(navOpen, value => {
+      if (value) showNav.value = value;
+      else {
+        setTimeout(() => {
+          showNav.value = value;
+        }, 300);
+      }
+    });
+
+    return { showNav };
+  }
+};
+</script>
