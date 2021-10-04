8ff8f6354ed1109c79900f5ee03e330ef0618f0a
Jason Redick <jason.redick@me.com>
2021-10-04 11:38:02 -0700

    start homepage and hero section

---
+++ components/content/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-white">
+    <div class="relative bg-gray-900">
+      <div v-if="section.image" aria-hidden="true" class="absolute inset-0 overflow-hidden">
+        <img :src="section.image.src" :alt="section.image.alt" class="w-full h-full object-center object-cover">
+      </div>
+      <div aria-hidden="true" class="absolute inset-0 bg-gray-900 opacity-50"></div>
+
+      <div class="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0">
+        <h1 v-if="section.heading" class="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">{{ section.heading }}</h1>
+        <p v-if="section.text" class="mt-4 text-xl text-white">{{ section.text }}</p>
+        <nuxt-link v-if="section.cta.label && section.cta.url" :to="section.cta.url" class="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100">{{ section.cta.label }}</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    section: {
+      type: Object,
+      default: (() => {})
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,48 @@
 <template>
-  <div>Homepage</div>
+  <main>
+    <div v-for="(section, index) in page.sections" :key="index">
+      <component :is="section.type" :section="section" />
+    </div>
+  </main>
 </template>
+
+<script>
+import Hero from "~/components/content/Hero.vue";
+import { ref, useFetch } from "@nuxtjs/composition-api";
+// import { useSpaceProvider } from "@nacelle/vue";
+
+const content = {
+  homepage: {
+    sections: [
+      {
+        type: "hero",
+        image: {
+          src: "https://tailwindui.com/img/ecommerce-images/home-page-01-hero-full-width.jpg",
+          alt: "hero image"
+        },
+        heading: "New arrivals are here",
+        text: "The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they're still in stock.",
+        cta: {
+          label: "Shop New Arrivals",
+          url: "/"
+        }
+      }
+    ]
+  }
+};
+
+export default {
+  components: {
+    Hero
+  },
+  setup() {
+    // const { nacelleSdk } = useSpaceProvider();
+    const page = ref({});
+    const { fetchState } = useFetch(async () => {
+      // page.value = await nacelleSdk.data.page({ handle: "homepage" });
+      page.value = content["homepage"];
+    });
+    return { page, fetchState };
+  }
+}
+</script>
