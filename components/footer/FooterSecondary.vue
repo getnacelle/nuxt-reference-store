af8d703fe746ade6d4e3c44d51afbda5f609a81f
Alex Asche <alexcasche@gmail.com>
2021-10-05 10:25:55 -0400

    props to provide/inject

--- components/footer/FooterSecondary.vue
+++ components/footer/FooterSecondary.vue
@@ -1,10 +1,11 @@
 <template>
   <div
+    v-if="content"
     class="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between"
   >
     <div class="flex space-x-6 md:order-2">
       <a
-        v-for="(account, index) in social"
+        v-for="(account, index) in content.social"
         :key="index"
         :href="account.url"
         class="text-gray-400 hover:text-gray-500"
@@ -16,21 +17,24 @@
         />
       </a>
     </div>
-    <p class="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
-      &copy; {{ year }} {{ copyright }}
+    <p
+      v-if="content.copyright"
+      class="mt-8 text-base text-gray-400 md:mt-0 md:order-1"
+    >
+      &copy; {{ year }} {{ content.copyright }}
     </p>
   </div>
 </template>
 
 <script>
-import footerData from "~/data/footer";
+import { inject } from "@nuxtjs/composition-api";
 
 export default {
+  name: "FooterSecondary",
   setup() {
     const year = new Date().getFullYear();
     return {
-      social: footerData.social,
-      copyright: footerData.copyright,
+      content: inject("secondary"),
       year
     };
   }
