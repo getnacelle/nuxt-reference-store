b3f2a3d246e9684188e6bd3686d116cd276c6794
Jason Redick <jason.redick@me.com>
2021-10-07 12:08:11 -0700

    add side by side section

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
 
 <script>
 import Hero from "~/components/sections/Hero.vue";
+import SideBySide from "~/components/sections/SideBySide.vue";
 import { ref, useFetch } from "@nuxtjs/composition-api";
 // import { useSpaceProvider } from "@nacelle/vue";
 
@@ -26,14 +27,45 @@ const content = {
           label: "Shop New Arrivals",
           url: "/"
         }
-      }
+      },
+      {
+        type: "sideBySide",
+        layout: "image-left",
+        image: {
+          src: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+          alt: "side by side image"
+        },
+        heading: "Our Process",
+        subheading: "Work with us",
+        text: "Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.",
+        cta: {
+          label: "Get Started",
+          url: "/",
+        },
+      },
+      {
+        type: "sideBySide",
+        layout: "image-right",
+        image: {
+          src: "https://tailwindui.com/img/ecommerce-images/home-page-01-hero-full-width.jpg",
+          alt: "side by side image"
+        },
+        heading: "New Arrivals Are Here",
+        subheading: "",
+        text: "The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they're still in stock.",
+        cta: {
+          label: "Shop New Arrvials",
+          url: "/",
+        },
+      },
     ]
   }
 };
 
 export default {
   components: {
-    Hero
+    Hero,
+    SideBySide
   },
   setup() {
     // const { nacelleSdk } = useSpaceProvider();
