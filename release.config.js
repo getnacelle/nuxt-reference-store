7fe237167daf4b5aae3eaa191b046f1fc0fa2a78
Alex Asche <alexcasche@gmail.com>
2021-10-12 09:47:03 -0400

    remove semantic release config

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: ["main", "ll-1035_versioning"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ],
-  dryRun: true
-};
