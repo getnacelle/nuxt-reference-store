9e0507f1ac982356233309ceeaa2fb2c31e61122
Nick Richmond <nick@getnacelle.com>
2021-10-19 18:22:00 -0400

    style: prevent cart actions when checking out

--- components/cart/CartCrossSells.vue
+++ components/cart/CartCrossSells.vue
@@ -5,26 +5,44 @@
     </h2>
     <ul>
       <li v-for="item in activeCrossSells" :key="item.id" class="py-6 flex">
-        <div class="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
-          <nuxt-img :src="item.variants[0].featuredMedia.src" :alt="item.variants[0].featuredMedia.alt" class="w-full h-full object-center object-cover" />
+        <div
+          class="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden"
+        >
+          <nuxt-img
+            :src="item.variants[0].featuredMedia.src"
+            :alt="item.variants[0].featuredMedia.alt"
+            class="w-full h-full object-center object-cover"
+          />
         </div>
 
         <div class="ml-4 flex-1 flex flex-col">
           <div>
-            <div class="flex justify-between text-base font-medium text-gray-900">
+            <div
+              class="flex justify-between text-base font-medium text-gray-900"
+            >
               <h3>
                 <nuxt-link :to="`/products/${item.handle}`">
                   {{ item.title }}
                 </nuxt-link>
               </h3>
               <p class="ml-4">
-                <price :price="item.variants[0].price" :currencyCode="item.priceRange.currencyCode" :locale="item.locale" />
+                <price
+                  :price="item.variants[0].price"
+                  :currencyCode="item.priceRange.currencyCode"
+                  :locale="item.locale"
+                />
               </p>
             </div>
           </div>
           <div class="flex-1 flex items-end text-sm">
             <div class="flex w-full">
-              <button type="button" @click="addProduct(item)" class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full">{{ content.add }}<span class="sr-only">, {{ item.title }}</span></button>
+              <button
+                type="button"
+                @click="addProduct(item)"
+                class="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200 w-full"
+              >
+                {{ content.add }}<span class="sr-only">, {{ item.title }}</span>
+              </button>
             </div>
           </div>
         </div>
@@ -48,17 +66,25 @@ export default {
     const { cart, addItem } = useCartProvider();
     const crossSells = ref([]);
     const content = inject("crosssells");
+    const isCheckingOut = inject("isCheckingOut");
 
-    const addProduct = (product) => {
-      addItem({ product, variant: product.variants[0], quantity: 1 });
+    const addProduct = product => {
+      if (!isCheckingOut.value) {
+        addItem({ product, variant: product.variants[0], quantity: 1 });
+      }
     };
 
     const activeCrossSells = computed(() => {
-      return crossSells.value.filter((crossSell) => {
-        return crossSell.availableForSale && !cart.lineItems.some((lineItem) => {
-          return lineItem.product.id === crossSell.id;
+      return crossSells.value
+        .filter(crossSell => {
+          return (
+            crossSell.availableForSale &&
+            !cart.lineItems.some(lineItem => {
+              return lineItem.product.id === crossSell.id;
+            })
+          );
         })
-      }).slice(0, 3);
+        .slice(0, 3);
     });
 
     useFetch(async () => {
@@ -69,5 +95,5 @@ export default {
 
     return { content, addProduct, crossSells, activeCrossSells };
   }
-}
+};
 </script>
--- components/cart/CartOverlay.vue
+++ components/cart/CartOverlay.vue
@@ -1,6 +1,11 @@
 <template>
   <transition name="fade">
-    <div v-show="cartOpen" @click="setCartOpen(false)" class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity ease-in-out duration-500" aria-hidden="true"></div>
+    <div
+      v-show="cartOpen"
+      @click="closeCart"
+      class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity ease-in-out duration-500"
+      aria-hidden="true"
+    ></div>
   </transition>
 </template>
 
@@ -12,9 +17,16 @@ export default {
   setup() {
     const cartOpen = inject("cartOpen");
     const setCartOpen = inject("setCartOpen");
-    return { cartOpen, setCartOpen };
+    const isCheckingOut = inject("isCheckingOut");
+    const closeCart = () => {
+      if (!isCheckingOut.value) {
+        setCartOpen(false);
+      }
+    };
+
+    return { cartOpen, setCartOpen, closeCart };
   }
-}
+};
 </script>
 
 <style scoped>
